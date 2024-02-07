5d3cccd3bd3dc8d9a2126a8d1020d0e7171f4abb
Zorigoo-1216 <143978750+Zorigoo-1216@users.noreply.github.com>
2024-02-08 01:57:20 +0800

    kk

--- components/CartComponent.js
+++ components/CartComponent.js
@@ -1,13 +1,15 @@
 class CartComponent extends HTMLElement {
 
-    constructor() {
-        super();
-        this.productsList = [];
-        this.render();
+    constructor() {                 //Байгуулагч 
+
+        super();                    // Эх классын байгуулагч 
+        this.productsList = [];      // сагсанд нэмсэн бүтээгдэхүүнүүдийг хадгалах масссив
+        this.render();                 //Анхны интерфейсыг байгуулах үеп дуудна
     }
 
-    render() {
-        this.innerHTML = `
+    render() {                          // сагсны icon ийг энд render-лэнэ
+        // Set the innerHTML property with the component's HTML template
+        this.innerHTML = `              
             <style>
                 .cart-container {
                     display: flex;
@@ -35,28 +37,35 @@ class CartComponent extends HTMLElement {
             
         `
     }
-
+    // сагсанд бүтээглэхүүн нэмэх функц
     addToCart(product) {
+        // product-ынг productsList массив руу нэмнэ үү
         this.productsList.push(product);
+        // Шинэчлэгдсэн product-ын жагсаалтыг localStorage-ийн "сагс" түлхүүрт хадгална
         localStorage.setItem("cart", JSON.stringify(this.productsList));
+        // Render хийнэ
         this.render();
     }
-
+    // LocalStorage-аас product-ын жагсаалтыг авах арга
     readFromLocalStorage() {
+        // Локал сангаас "cart" гэсэн нэртэй объект унших
         const _productsList = JSON.parse(localStorage.getItem("cart"));
+        // Үр дүнг шалгах
         if (_productsList != null) {
+            // Бараа нийлүүлсэн массиваа өөрчлөх
             this.productsList = _productsList;
+            // Интерфэйсээ шинэчлэх
             this.render();
         }
     }
-
+    // Элемент нэмэгдсэн бол ажиллах функц
     connectedCallback() {
-
+        // Локал сангаас унших функц дуудах
         this.readFromLocalStorage();
 
     }
 
 }
-
+// "cart-component" нэртэй хэрэглэгчийн элементийг тодорхойлох
 customElements.define("cart-component", CartComponent);
 
